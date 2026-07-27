9d18180dd6e97522ac0a160badca61d39c27ef0a
Ahmed Shah <ahmedshawgi766@gmail.com>
2026-07-27 21:01:02 +0300

    Create typedef.c

---
+++ typedef.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+//it declared out side of da main funtion 
+//u can rename to what ever u wont ; )
+
+typedef int num;
+
+int main() {
+
+    //give a nickname for a data type in C
+
+    num x = 3;
+    num y = 4;
+    num z = x + y;
+
+    printf("%d", z);
+
+
+    return 0;
+}
