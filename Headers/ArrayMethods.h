6602816e9072961896786d43f81802be5e1c92d3
Ahmed Shah <ahmedshawgi766@gmail.com>
2026-08-02 15:42:07 +0300

    Array Methods Lib

---
+++ Headers/ArrayMethods.h
@@ -0,0 +1,41 @@
+#ifndef ARRAY_METHODS
+#define ARRAY_METHODS
+#include <stddef.h>
+#include <stdbool.h>
+
+#define ArrayLength(arr) (sizeof(arr) / sizeof(arr[0]))
+
+#define ArrayForEtch(arr, fn) ( \
+    do{ \
+        size_t len = ArrayLength(arr); \
+        for(size_t i = 0; i < len; i++){ \
+        fn(arr[i]) \
+        } \
+    } while(0) \
+)
+
+#define ArrayHas(arr, value) ({ \
+    bool found = false; \
+    size_t len = ArrayLength(arr); \
+    for(size_t i = 0; i < len; i++) { \
+        if(arr[i] == value) { \
+            found = true; \
+            break; \
+        } \
+    } \
+    found; \
+})
+
+#define IndexOf(arr, value) ({ \
+    int index = -1; \
+    size_t len = ArrayLength(arr); \
+    for(size_t i = 0; i < len; i++){ \
+        if(arr[i] == value) { \
+            index = (int)i; \
+            break; \
+        } \
+    } \
+    index; \
+}) 
+
+#endif
